56e1cc038e799c76d46d8e1a30a669445fe1977b
Satyamray21 <satyamray43@gmail.com>
2023-11-24 15:21:41 +0530

    change in UserController

--- src/main/java/com/example/simplebackend/Controller/UserController.java
+++ src/main/java/com/example/simplebackend/Controller/UserController.java
@@ -1,20 +1,37 @@
 package com.example.simplebackend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.simplebackend.Model.UserModel;
+import com.example.simplebackend.Service.UserService;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/register")
-    public String getRegisterPage()
-    {
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerRequest", new UserModel());
         return "register_Page";
     }
+
     @GetMapping("/login")
-    
-        public String getLoginPage()
-        {
-            return "login_Page";
-        }
+    public String getLoginPage(Model model) {
+        model.addAttribute("loginRequest", new UserModel());
+        return "login_Page";
     }
 
+    @PostMapping("/register")
+    public String register(@ModelAttribute UserModel userModel) {
+        System.out.println("register request: " + userModel);
+        UserModel registeredUser = userService.registerUser(userModel.getLogin(), userModel.getPassword(), userModel.getEmail());
+        // You should add some logic here for handling the registered user
+        return "redirect:/login"; 
+    }
+}
